efd18c9e72c2b7a153bd5139929e64ad41cabad1
jacekpietrzak <jacekpietrzak@me.com>
2023-04-14 14:15:44 +0200

    hw05 finished
    - avatarURL for user model added
    - generating gravatar and saving in avatarURL after user signup
    - user avatar update route added

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const app = require("./app");
+const fs = require("fs").promises;
+const path = require("path");
+const uploadDir = path.join(process.cwd(), "tmp");
+const storeImage = path.join(process.cwd(), "/public/avatars");
 
 const { connectToDatabase } = require("./startup/database.js");
 connectToDatabase();
 
+const isAccessible = (path) => {
+  return fs
+    .access(path)
+    .then(() => true)
+    .catch(() => false);
+};
+
+const createFolderIsNotExist = async (folder) => {
+  if (!(await isAccessible(folder))) {
+    await fs.mkdir(folder);
+  }
+};
+
 const port = process.env.PORT;
+
 app.listen(port, () => {
+  createFolderIsNotExist(uploadDir);
+  createFolderIsNotExist(storeImage);
   console.log(`Server running. Use our API on port: ${port}`);
 });
